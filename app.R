60249bd8a5712bc7fad6c7d1aadf27a7da022b97
Tijmen Pauli <tijmenpauli@Tijmens-MacBook-Air-2.local>
2022-03-02 16:02:21 +0100

    htfuj

--- app.R
+++ app.R
@@ -36,6 +36,7 @@ renderPlot({
 })
 ```
 Tijmen
+Pauli
 ## Embedded Application
 
 It's also possible to embed an entire Shiny application within an R Markdown document using the `shinyAppDir` function. This example embeds a Shiny application located in another directory:
