71e4cb9f303dccd85914b35f6bca4c9ff5e45d34
rijck <r.ijck@hotmail.com>
2022-03-03 16:40:51 +0100

    created the basics for the User Interface

--- ui.R
+++ ui.R
@@ -1,3 +1,17 @@
 ## ui.R ##
 
 library(shinydashboard)
+
+shinyUI(dashboardPage(
+  dashboardHeader(title = "Olympics Network Analytics"),
+  dashboardSidebar(
+    sidebarUserPanel("NDA Group 1"),
+    
+    sidebarMenu(
+      menuItem("Homepage", tabName = "hp", icon = icon("home")),
+      
+      menuItem("Descriptive Statistics", tabName = "ds",
+               menuSubItem()
+    )
+  )
+)))
