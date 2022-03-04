2ae63b2984760d4a86fa78b26cf64cdf96522320
rijck <r.ijck@hotmail.com>
2022-03-04 09:29:53 +0100

    made a few minor steps

--- ui.R
+++ ui.R
@@ -8,10 +8,24 @@ shinyUI(dashboardPage(
     sidebarUserPanel("NDA Group 1"),
     
     sidebarMenu(
-      menuItem("Homepage", tabName = "hp", icon = icon("home")),
+      menuItem(
+        "Homepage",
+         tabName = "hp", icon = icon("home")),
       
-      menuItem("Descriptive Statistics", tabName = "ds",
-               menuSubItem()
-    )
+      menuItem(
+        "Descriptive Statistics",
+         tabName = "ds",
+         menuSubItem("Description of Dataset", tabName = 'dd'),
+         menuSubItem("Summary Statistics", tabName = "ss")),
+      
+      menuItem(
+        "Network Exploratiion", 
+         tabName = "ne",
+         menuSubItem("Network Visualization", tabName = "nv"),
+         menuSubItem("Network Descriptive Statistics", tabName = "nds")),
+      
+      menuItem(
+        "About the Creators",
+         tabName = "atc")
   )
 )))
