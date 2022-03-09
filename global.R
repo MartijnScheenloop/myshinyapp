5fd93c65a4aa2111b4bc1ac13b90197c1d8547c6
rijck <r.ijck@hotmail.com>
2022-03-09 10:28:57 +0100

    made some code changes

--- global.R
+++ global.R
@@ -20,23 +20,25 @@ dt.olympics <- merge(dt.athletes.events, dt.regions[, list(NOC, region)],
                      by = 'NOC', 
                      all = TRUE)
 
+
 # New data table containing medals per country
-country_medals = dt.olympics %>% na.omit() %>% group_by(., region) %>% count(., Medal)
-colnames(country_medals)[1] = "Country"
-country_medals$Country = as.character(country_medals$Country)
-head(country_medals)
-country_medals$Country
+dt.country.medals = dt.olympics %>% na.omit() %>% group_by(., region) %>% count(., Medal)
 
-country_medals[country_medals$Country == "USA", ]$Country = "United States"
-country_medals$Country
+# Change column name for rworldmap
+colnames(dt.country.medals)[1] = "Country"
+dt.country.medals$Country = as.character(dt.country.medals$Country)
+dt.country.medals$Country
 
-country_medals$s_medals = paste0(country_medals$Medal, " : ", country_medals$n)
+# Change name of USA to match rworldmap name
+dt.country.medals[dt.country.medals$Country == "USA", ]$Country = "United States"
+dt.country.medals[dt.country.medals$Country == "UK", ]$Country = "United Kingdom"
+dt.country.medals$Country
 
-country_medals_final = country_medals %>%
+# create and add string of medals to dt.country.medals
+dt.country.medals$s_medals = paste0(dt.country.medals$Medal, " : ", dt.country.medals$n)
+
+dt.country.medals.final = dt.country.medals %>%
   group_by(., Country) %>%
   mutate(., country_medals_string = paste0(s_medals, collapse = " ")) %>%
   select(., country_medals_string, Country) %>%
   unique()
-
-country_medals_final
-
