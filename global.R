00cae618a72736a602ba63119a55d9403840e299
rijck <r.ijck@hotmail.com>
2022-03-09 12:45:23 +0100

    made changes to the worldmap

--- global.R
+++ global.R
@@ -23,22 +23,25 @@ dt.olympics <- merge(dt.athletes.events, dt.regions[, list(NOC, region)],
                      all = TRUE)
 
 # Replace NAs in region for dt.olympics
-# NOC of Singapore was changed from SIN to SGP and USA to United States
+# NOC of Singapore was changed from SIN to SGP
 # and UK to United Kingdom 
 # This was not included in dt.regions
 dt.olympics[NOC == "SGP"]$region = "Singapore"
+
+# Change USA and UK to match rworldmap package
 dt.olympics[NOC == "USA"]$region = "United States"
 dt.olympics[NOC == "UK"]$region = "United Kingdom"
 
 # Remove rows with NA as Event, are useless
 dt.olympics <- dt.olympics[!is.na(Event)]
 
+
 # New data table containing medals per country
 dt.country.medals = na.omit(dt.olympics)
 dt.country.medals = group_by(dt.country.medals, region)
 dt.country.medals = count(dt.country.medals, Medal)
 
-# Change column name for rworldmap
+# Change column name for rworldmap compatability
 colnames(dt.country.medals)[1] = "Country"
 dt.country.medals$Country = as.character(dt.country.medals$Country)
 dt.country.medals$Country
