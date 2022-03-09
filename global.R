2e6ee2b34ca9ac6266ca55e894bef79703f9d34c
rijck <r.ijck@hotmail.com>
2022-03-09 12:06:44 +0100

    code changes

--- global.R
+++ global.R
@@ -10,7 +10,6 @@ library(rworldmap)
 library(googleVis)
 library(dplyr)
 
-
 # Load the data
 dt.athletes.events <- fread("athlete_events.csv")
 dt.regions <- fread("noc_regions.csv")
@@ -24,17 +23,15 @@ dt.olympics <- merge(dt.athletes.events, dt.regions[, list(NOC, region)],
                      all = TRUE)
 
 # Replace NAs in region for dt.olympics
-# NOC of Singapore was changed from SIN to SGP
+# NOC of Singapore was changed from SIN to SGP and USA to United States
+# and UK to United Kingdom 
 # This was not included in dt.regions
 dt.olympics[NOC == "SGP"]$region = "Singapore"
-<<<<<<< HEAD
 dt.olympics[NOC == "USA"]$region = "United States"
 dt.olympics[NOC == "UK"]$region = "United Kingdom"
-=======
 
 # Remove rows with NA as Event, are useless
 dt.olympics <- dt.olympics[!is.na(Event)]
->>>>>>> fea64a25da6c964c0cebee978b06f381d50e2145
 
 # New data table containing medals per country
 dt.country.medals = na.omit(dt.olympics)
@@ -49,8 +46,11 @@ dt.country.medals$Country
 # create and add string of medals to dt.country.medals
 dt.country.medals$s_medals = paste0(dt.country.medals$Medal, " : ", dt.country.medals$n)
 
-dt.country.medals.final = dt.country.medals %>%
-  group_by(., Country) %>%
-  mutate(., country_medals_string = paste0(s_medals, collapse = " ")) %>%
-  select(., country_medals_string, Country) %>%
-  unique()
+dt.country.medals.final = group_by(dt.country.medals, Country)
+dt.country.medals.final = mutate(dt.country.medals.final,
+                                 country_medals_string = paste0(s_medals, collapse = " "))
+dt.country.medals.final = select(dt.country.medals.final, country_medals_string, Country)
+dt.country.medals.final = unique(dt.country.medals.final)
+
+
+
--- server.R
+++ server.R
@@ -51,7 +51,9 @@ shinyServer(function(input, output, body){
     
   }))
   
-  dt.country.medals.final$country_medals_string = paste(dt.country.medals.final$Country[1:133], dt.country.medals.final$country_medals_string[1:133], sep = " - ")
+  dt.country.medals.final$country_medals_string =
+        paste(dt.country.medals.final$Country[1:133],
+            dt.country.medals.final$country_medals_string[1:133], sep = " - ")
   
   output$wm = renderGvis ( {
     gvisGeoChart(
